15f774a0c6e4b936d9c75896e944c836f55e01b8
Michael Stengel <mik..stengel@gmail.com>
2012-09-10 01:09:50 -0400

    Configurations
    Added Configurations for blockId's and ItemIds.
    Also fixed modid seeing it had spaces, causing issues.

--- common/EnterTheVoid.java
+++ common/EnterTheVoid.java
@@ -1,19 +1,27 @@
 package enterTheVoid.common;
 
+import java.util.logging.Level;
+
+import odin.mods.AdvancedEnchanting.common.AdvEnchantment;
 import net.minecraft.src.Block;
 import net.minecraft.src.EnumToolMaterial;
+import net.minecraftforge.common.Configuration;
 import net.minecraftforge.common.EnumHelper;
 import net.minecraft.src.Item;
+import cpw.mods.fml.common.FMLLog;
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.Mod.Init;
+import cpw.mods.fml.common.Mod.Instance;
+import cpw.mods.fml.common.Mod.PreInit;
 import cpw.mods.fml.common.SidedProxy;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 import cpw.mods.fml.common.network.NetworkMod;
 import cpw.mods.fml.common.registry.GameRegistry;
 import cpw.mods.fml.common.registry.LanguageRegistry;
 import enterTheVoid.common.CommonProxyEnterTheVoid;
 
-@Mod(modid = "Enter: The Void", name = "Enter: The Void", version = "0.0")
+@Mod(modid = "EnterTheVoid", name = "Enter: The Void", version = "0.0")
 @NetworkMod(clientSideRequired = true, serverSideRequired  = false)
 
 public class EnterTheVoid {
@@ -21,7 +29,7 @@ public class EnterTheVoid {
 	//Blocks
 
 	public static net.minecraft.src.Block Voidstone;
-	public static int VoidstoneBlockID = 210;
+	public static int VoidstoneBlockID;
 
 	//Items
 
@@ -31,10 +39,34 @@ public class EnterTheVoid {
 	public static Item VoidSword;
 	public static Item RealSwordBlade;
 	public static Item RealSword;
-
+	
+	public static int VoidstuffItemId;
+	public static int RealstuffItemId;
+	public static int VoidSwordBladeItemId;
+	public static int VoidSwordItemId;
+	public static int RealSwordBladeItemId;
+	public static int RealSwordItemId;
+	
 	@SidedProxy(clientSide = "enterTheVoid.client.ClientEnterTheVoid", serverSide = "enterTheVoid.common.CommonProxyEnterTheVoid")
 	public static CommonProxyEnterTheVoid proxy;
-
+	
+	@PreInit
+	public void preInit(FMLPreInitializationEvent event) {
+		Configuration config = new Configuration(event.getSuggestedConfigurationFile());
+		try {
+			config.load();
+			VoidstoneBlockID = config.getOrCreateBlockIdProperty("Voidstuff", 210).getInt(210);
+			VoidstuffItemId = config.getOrCreateIntProperty("VoidStuff", "item", 5000).getInt(5000);
+			RealstuffItemId = config.getOrCreateIntProperty("RealStuff", "item", 5001).getInt(5001);
+			VoidSwordBladeItemId = config.getOrCreateIntProperty("Voidstuff Sword Blade", "item", 5002).getInt(5002);
+			
+		} catch (Exception e) {
+			FMLLog.log(Level.SEVERE, e, "Enter the Void failed to load configurations.");
+		} finally {
+			config.save();
+		}
+	}
+	
 	@Init
 	public void load(FMLInitializationEvent event)
 	{
@@ -49,9 +81,9 @@ public class EnterTheVoid {
 
 		//Pieces of Matter
 
-		Voidstuff = new VoidItemVoidstuff(5000).setIconIndex(0).setItemName("Voidstuff");
+		Voidstuff = new VoidItemVoidstuff(VoidstuffItemId).setIconIndex(0).setItemName("Voidstuff");
 		LanguageRegistry.addName(Voidstuff, "Voidstuff");
-		Realstuff = new VoidItemRealstuff(5001).setIconIndex(1).setItemName("Realstuff");
+		Realstuff = new VoidItemRealstuff(RealstuffItemId).setIconIndex(1).setItemName("Realstuff");
 		LanguageRegistry.addName(Realstuff, "Realstuff");
 
 		//Tools
@@ -62,7 +94,7 @@ public class EnterTheVoid {
 
 		//Tier 0
 
-		VoidSwordBlade = new ItemSwordBlade(5002, MatVoidstuff).setIconIndex(193).setItemName("Voidstuff Sword Blade");
+		VoidSwordBlade = new ItemSwordBlade(VoidSwordBladeItemId, MatVoidstuff).setIconIndex(193).setItemName("Voidstuff Sword Blade");
 
 		//Proxy
 
